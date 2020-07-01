86451f36da0c0bd381cfc5e3c4cc268ed6cf2704
tonmoysaha <tonmoysahaopi@gmail.com>
2020-06-30 21:19:00 -0700

    modifying repository

--- src/main/java/com/fullstack/springboot/config/RestConfig.java
+++ src/main/java/com/fullstack/springboot/config/RestConfig.java
@@ -1,5 +1,13 @@
 package com.fullstack.springboot.config;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -10,6 +18,14 @@ import com.fullstack.springboot.entity.ProductCategory;
 
 @Configuration
 public class RestConfig implements RepositoryRestConfigurer {
+	
+	private EntityManager entityManager;
+
+	@Autowired
+	public RestConfig(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
 
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
@@ -30,8 +46,25 @@ public class RestConfig implements RepositoryRestConfigurer {
 		.withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedAction))
 		.withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedAction));
 		
+		exposeIds(config);
 		
 	
+	}
+
+
+	private void exposeIds(RepositoryRestConfiguration config) {
+		
+		Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+		
+		List<Class> Entityclasses = new ArrayList<>();
+		
+		for (EntityType entityclass : entities) {
+			Entityclasses.add(entityclass.getJavaType());
+		}
+		
+		Class[] domaintype = Entityclasses.toArray(new Class[0]);
+		config.exposeIdsFor(domaintype);
+		
 	}
 
 	
--- src/main/java/com/fullstack/springboot/repository/ProductRepository.java
+++ src/main/java/com/fullstack/springboot/repository/ProductRepository.java
@@ -1,10 +1,16 @@
 package com.fullstack.springboot.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.fullstack.springboot.entity.Product;
+
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+	
+	Page<Product> findByCategoryId(@RequestParam("id") Long id , Pageable pageable);
+	Page<Product> findByNameContainingIgnoreCase(@RequestParam("name") String name , Pageable pageable );
 }
