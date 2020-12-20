e4fb72da3b0c60156bb0c999bcbeb5331d4c04b4
tonmoysaha <tonmoysahaopi@gmail.com>
2020-12-20 03:42:01 -0800

    implement place order

--- src/main/java/com/fullstack/springboot/serviceimpl/CheckoutServiceImpl.java
+++ src/main/java/com/fullstack/springboot/serviceimpl/CheckoutServiceImpl.java
@@ -1,9 +1,15 @@
 package com.fullstack.springboot.serviceimpl;
 
+import java.util.Set;
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
 import com.fullstack.springboot.dto.Purchase;
 import com.fullstack.springboot.dto.PurchaseResponse;
+import com.fullstack.springboot.entity.Customer;
+import com.fullstack.springboot.entity.Order;
+import com.fullstack.springboot.entity.OrderItem;
 import com.fullstack.springboot.repository.CustomerRepository;
 import com.fullstack.springboot.service.CheckoutService;
 
@@ -18,11 +24,32 @@ public class CheckoutServiceImpl implements CheckoutService {
 	}
 
 
-
 	@Override
 	public PurchaseResponse placeOrder(Purchase purchase) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Order order = purchase.getOrder();
+		
+		Set<OrderItem> orderItems = purchase.getOrderItems();
+		orderItems.forEach(item -> order.add(item));
+		
+		String orderTrackingNumber = generateOrderTrackingNumber();
+		order.setOrderTrackingNumber(orderTrackingNumber);
+		
+		order.setBillingAddress(purchase.getBillingAddress());
+		order.setShippingAddress(order.getShippingAddress());
+		
+		Customer customer = purchase.getCustomer();
+		customer.add(order);
+		
+		this.customerRepository.save(customer);
+		
+		return new PurchaseResponse(orderTrackingNumber);
+	}
+
+
+	private String generateOrderTrackingNumber() {
+		
+		return UUID.randomUUID().toString();
 	}
 
 }
